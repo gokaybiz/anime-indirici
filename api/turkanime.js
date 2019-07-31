c8c72ec8ce81d2bc9146575559bffc78dffec4c4
gokaybiz <gkymhmt@gmail.com>
2019-07-31 14:13:38 +0300

    Cesitli iyilestirmeler yapildi

--- api/turkanime.js
+++ api/turkanime.js
@@ -1,63 +1,62 @@
-const axios = require('axios');
-
-let headers = {
+const axios = require('axios')
+const headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Language': 'tr-TR,tr;q=0.8,en-US;q=0.5,en;q=0.3',
     'Content-Type': 'application/x-www-form-urlencoded',
-    'Alt-Used': 'www.turkanime.tv:443',
-    'Connection': 'keep-alive',
     'Referer': 'http://www.turkanime.tv/',
     'Upgrade-Insecure-Requests': '1',
     'TE': 'Trailers',
-    'Cookie': " _ga=GA1.2.1278321181.1564136960; _gat=1; _gid=GA1.2.1445927356.1563524213; PHPSESSID=tqpqo4smru3mhfej4pdkqf85n0; __cfduid=dec57aab208a3f67382bb2ed120c0ef081564136948"
+    'Cookie': "PHPSESSID=tqpqo4smru3mhfej4pdkqf85n0; __cfduid=dec57aab208a3f67382bb2ed120c0ef081564136948"
 
-};
-
-let headersAjax = { ...headers, ...{ "X-Requested-With": "XMLHttpRequest" } }
-
+}
+const headersAjax = {
+    ...headers,
+    ...{ "X-Requested-With": "XMLHttpRequest" }
+}
+const ta = axios.create({
+    baseURL: 'http://www.turkanime.tv/',
+    headers: headers
+})
 
 async function animeAra(ara) {
-
     try {
-        const response = await axios.post('http://www.turkanime.tv/arama', `arama=${encodeURIComponent('ara')}`, { "headers": headers });
-        console.log(response.data);
+        const {data} = await ta.post('arama', `arama=${encodeURIComponent('ara')}`)
+        console.log(data)
     } catch (error) {
-        console.error(error);
+        console.error(error)
     }
 }
-let _animeListesi = undefined;//sonraki zamanlar için cache
+let _animeListesi = undefined //sonraki zamanlar için cache
 async function animeListesi() {
-    let regs = new RegExp('<a href="\/\/www\.turkanime\.tv\/anime\/(.*?)"(.*?)><span class="animeAdi">(.*?)<\/span><span class="kategori" style="display:none;">(.*?)<\/span>', 'ig');
+    let regs = new RegExp('<a href="\/\/www\.turkanime\.tv\/anime\/(.*?)"(.*?)><span class="animeAdi">(.*?)<\/span><span class="kategori" style="display:none;">(.*?)<\/span>', 'ig')
     try {
-        const response = await axios.get('http://www.turkanime.tv/ajax/tamliste', { "headers": headersAjax });
-        // console.log(response.data);
-        //console.log(response.data.match(regs).groups)
-        let dict = new Map();
-        while (match = regs.exec(response.data)) {
+        const {data} = await ta.get('ajax/tamliste', { headers: headersAjax })
+        let dict = new Map()
+        while (match = regs.exec(data))
             dict.set(match[3], match[1])
-        }
-        return dict;
+
+        return dict
     } catch (error) {
-        console.error(error);
+        console.error(error)
     }
 }
 
 async function bolumListesi(slug) {
-    var regs = new RegExp('<a href="\/\/www\.turkanime\.tv\/video\/(.*?)" (.*?)><span class="bolumAdi">(.*?)<\/span><\/a>', 'ig')
+    let regs = new RegExp('<a href="\/\/www\.turkanime\.tv\/video\/(.*?)" (.*?)><span class="bolumAdi">(.*?)<\/span><\/a>', 'ig')
     try {
-        let response = await axios.get(`http://www.turkanime.tv/anime/${slug}`, { "headers": headers });
-        let animeId = response.data.split('ajax/bolumler&animeId=')[1].split('"')[0]
-        response = await axios.get(`http://www.turkanime.tv/ajax/bolumler&animeId=${animeId}`, { "headers": headersAjax });
+        let {data: animeId} = await ta.get(`anime/${slug}`)
+            animeId = animeId.split('ajax/bolumler&animeId=')[1].split('"')[0]
+        let {data: episodes} = await ta.get(`ajax/bolumler&animeId=${animeId}`, { headers: headersAjax })
 
-        let dict = new Map();
-        while (match = regs.exec(response.data)) {
+        let dict = new Map()
+        while (match = regs.exec(episodes))
             dict.set(match[3], match[1])
-        }
-        return dict;
+
+        return dict
     } catch (error) {
-        console.error(error);
+        console.error(error)
     }
 }
 
-module.exports = { "bolumListesi": bolumListesi, "animeAra": animeAra, "animeListesi": animeListesi };
+module.exports = {bolumListesi, animeAra, animeListesi}
--- main.js
+++ main.js
@@ -1,83 +1,43 @@
 
-const turkanime = require('./api/turkanime');
-const { AutoComplete } = require('enquirer');
-let inquirer = require('inquirer');
-let fuzzy = require('fuzzy');
-inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
+const turkanime = require('./api/turkanime')
+const inquirer = require('inquirer')
+const inquirerAutoComplete = require('./autocomplete')
 
-(async () => {
+inquirer.registerPrompt('autocomplete', inquirerAutoComplete)
 
-    let animeListesi = await turkanime.animeListesi();
-    function animeAra(answers, input) {
-        input = input || '';
-        return new Promise(function (resolve) {
-            setTimeout(function () {
-                var fuzzyResult = fuzzy.filter(input, Array.from(animeListesi.entries()), {
-                    extract: function (arg) {
-                        return arg[0];
-                    }
-                });
-
-                resolve(
-                    fuzzyResult.map(function (el) {
-                        return el.original[0];
-                    })
-                );
-            }, 1);
-        });
-    }
-    let anime = await inquirer
+;(async () => {
+    let animeListesi = await turkanime.animeListesi()
+    let animeSor = await inquirer
         .prompt([
             {
                 type: 'autocomplete',
-                name: 'anime',
+                name: 'secilenAnime',
                 suggestOnly: false,
                 message: 'Anime seçin',
-                source: animeAra,
+                searchIn: animeListesi,
                 pageSize: 20,
                 validate: function (val) {
-                    return val ? true : 'Type something!';
+                    return val ? true : 'Type something!'
                 },
             }
         ])
-    let slug = animeListesi.get(anime["anime"]);
-    let bolumListesi = await turkanime.bolumListesi(slug);
-    function bölümAra(answers, input) {
-        input = input || '';
-        return new Promise(function (resolve) {
-            setTimeout(function () {
-                var fuzzyResult = fuzzy.filter(input, Array.from(bolumListesi.entries()), {
-                    extract: function (arg) {
-                        return arg[0];
-                    }
-                });
+    let slug = animeListesi.get(animeSor.secilenAnime)
+    let bolumListesi = await turkanime.bolumListesi(slug)
 
-                resolve(
-                    fuzzyResult.map(function (el) {
-                        return el.original[0];
-                    })
-                );
-            }, 1);
-        });
-    }
-
-    let bölüm = await inquirer
+    let bolumSor = await inquirer
         .prompt([
             {
                 type: 'autocomplete',
-                name: 'bölüm',
+                name: 'secilenBolum',
                 suggestOnly: false,
                 message: 'Bölümü seçin',
-                source: bölümAra,
+                searchIn: bolumListesi,
                 pageSize: 20,
                 validate: function (val) {
-                    return val ? true : 'Type something!';
+                    return val ? true : 'Type something!'
                 },
             }
         ])
-    let bölümSlug = bolumListesi.get(bölüm["bölüm"]);
-    console.log(bölümSlug)
-
-
-
-})();
+    let bolumSlug = bolumListesi.get(bolumSor.secilenBolum)
+    console.log(bolumSlug)
+})()
