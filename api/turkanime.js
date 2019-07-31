58af42bb22c0a40ab51a8ba03c104b37ad3f76c1
gokaybiz <gkymhmt@gmail.com>
2019-07-31 21:15:08 +0300

    major degisiklikler yapildi, mpv player ile kullanilabilir hale getirildi.

--- api/turkanime.js
+++ api/turkanime.js
@@ -1,4 +1,20 @@
 const axios = require('axios')
+const CryptoJS = require('crypto-js')
+const CryptoJSAesJson = {
+    stringify: (cipherParams) => {
+        var j = {ct: cipherParams.ciphertext.toString(CryptoJS.enc.Base64)}
+        if (cipherParams.iv) j.iv = cipherParams.iv.toString()
+        if (cipherParams.salt) j.s = cipherParams.salt.toString()
+        return JSON.stringify(j)
+    },
+    parse: (jsonStr) => {
+        var j = JSON.parse(jsonStr);
+        var cipherParams = CryptoJS.lib.CipherParams.create({ciphertext: CryptoJS.enc.Base64.parse(j.ct)})
+        if (j.iv) cipherParams.iv = CryptoJS.enc.Hex.parse(j.iv)
+        if (j.s) cipherParams.salt = CryptoJS.enc.Hex.parse(j.s)
+        return cipherParams
+    }
+}
 const headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -15,10 +31,14 @@ const headersAjax = {
     ...{ "X-Requested-With": "XMLHttpRequest" }
 }
 const ta = axios.create({
-    baseURL: 'http://www.turkanime.tv/',
+    baseURL: 'https://www.turkanime.tv/',
     headers: headers
 })
-
+const mta = axios.create({
+    baseURL: 'https://m.turkanime.tv/',
+    headers: {...headers, ...{'Referer': 'https://m.turkanime.tv/',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36'}}
+})
 async function animeAra(ara) {
     try {
         const {data} = await ta.post('arama', `arama=${encodeURIComponent('ara')}`)
@@ -44,9 +64,10 @@ async function animeListesi() {
 
 async function bolumListesi(slug) {
     let regs = new RegExp('<a href="\/\/www\.turkanime\.tv\/video\/(.*?)" (.*?)><span class="bolumAdi">(.*?)<\/span><\/a>', 'ig')
+    let reg2 = new RegExp('data-url=\"ajax\/bolumler&animeId=(.*?)"', 'i')
     try {
         let {data: animeId} = await ta.get(`anime/${slug}`)
-            animeId = animeId.split('ajax/bolumler&animeId=')[1].split('"')[0]
+            animeId = animeId.match(reg2)[1]
         let {data: episodes} = await ta.get(`ajax/bolumler&animeId=${animeId}`, { headers: headersAjax })
 
         let dict = new Map()
@@ -59,4 +80,83 @@ async function bolumListesi(slug) {
     }
 }
 
-module.exports = {bolumListesi, animeAra, animeListesi}
+async function bolumVideolari(slug) {
+    const listeAl = (from, id) => (from.split(`<ul id="${id}" class="dropdown-content white-text blue-grey darken-3 z-depth-2">`)[1].split('</ul>')[0]).trim()
+
+    let kural = new RegExp('<li class="blue-grey darken-4 linkler"><a href="(.*?)" class="waves-effect waves-red white-text modal-trigger">(.*?)<\/a><\/li>', 'gi')
+    let kural2 = new RegExp('<li class="blue-grey darken-4 linkler"><a href="(.*?)" class="waves-effect waves-red white-text modal-trigger">(.*?)<\/a><\/li>', 'gi')
+    let iframeKural = new RegExp('iframe src="//(.*?)" ', 'i')
+    try {
+        let {data} = await axios.get(`https://m.turkanime.tv/video/${slug}`,{ headers: {...headers, ...{'Referer': 'https://m.turkanime.tv/',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36'}}})
+        let bolumVideoBase = data.match(new RegExp('data-slide="media-{ ajax: \'(.*?)\'', 'i'))[1]
+
+        let fansublar = listeAl(data, 'fansub')
+        let fansubListe = new Map() 
+
+        let eslesme
+        while ((eslesme = kural.exec(fansublar)) !== null) {
+            let fansub = eslesme[2]
+            let patika = eslesme[1]
+    
+            let {data} = await mta.get(encodeURI(patika))
+
+            let alternatifler = listeAl(data, 'kaynak')
+            let alternatifListe = new Map()
+            
+            let eslesme2
+            let istekHavuzu = [], isimHavuzu = []
+            while ((eslesme2 = kural2.exec(alternatifler)) !== null) {
+                isimHavuzu.push(eslesme2[2])
+                let id = eslesme2[1].split('=').pop()
+                
+                istekHavuzu.push(mta.get(bolumVideoBase + id, { headers: { "X-Requested-With": "XMLHttpRequest" } }))
+            }
+            /*while ((eslesme2 = kural2.exec(alternatifler)) !== null) {
+                let kaynak = eslesme2[2]
+                let id = eslesme2[1].split('=').pop()
+                
+                let {data} = await mta.get(bolumVideoBase + id, { headers: { "X-Requested-With": "XMLHttpRequest" } })
+
+                let link = data.match(iframeKural)[1]
+                if (link.includes('iframe')) {
+                    let {data} = await ta.get(link.replace('turkanime.tv/', ''))
+                    let sifre = data.match(/iframe = '(.*?)';/)[1]
+                    link = 'http:' + JSON.parse(`"${CryptoJS.AES.decrypt(sifre, '7Q+5&VnG1a{)-UWd)u$_}TiXINqCw|1HG,qfQvDgbK>W(O)m 2^B{5U|@+%tQ<;F', { format: CryptoJSAesJson }).toString(CryptoJS.enc.Utf8).slice(1, -1)}"`)
+                }
+
+                alternatifListe.set(kaynak, link)
+            }*/
+            axios.all(istekHavuzu)
+            .then(axios.spread(function (...istek) {
+                istek.forEach(async ({data}, i) => {
+                    let link = data.match(iframeKural)[1]
+                    if (link.includes('iframe')) {
+                        let iframeIstek = []
+                        iframeIstek.push(ta.get(link.replace('turkanime.tv/', '')))
+                        await axios.all(iframeIstek)
+                        .then(axios.spread(function (...iframeler) {
+                            iframeler.forEach(({data: sonuc}) => {
+                                let sifre = sonuc.match(/iframe = '(.*?)';/)[1]
+                                link = 'http:' + JSON.parse(`"${CryptoJS.AES.decrypt(sifre, '7Q+5&VnG1a{)-UWd)u$_}TiXINqCw|1HG,qfQvDgbK>W(O)m 2^B{5U|@+%tQ<;F', { format: CryptoJSAesJson }).toString(CryptoJS.enc.Utf8).slice(1, -1)}"`)
+                            })
+                        }))
+                    }
+                    
+                    if (alternatifListe.has(isimHavuzu[i])) {
+                        isimHavuzu[i] += ' ' + Math.floor(Math.random() * 10) //Ayni isimler ariza cikartmamali.
+                    }
+                    alternatifListe.set(isimHavuzu[i], link)
+                })
+            }))
+            fansubListe.set(fansub, alternatifListe)
+            // break
+            // console.log('calistim!', Math.random())
+        }
+        return fansubListe
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+module.exports = {bolumListesi, bolumVideolari, animeAra, animeListesi}
--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 
-const turkanime = require('./api/turkanime')
 const inquirer = require('inquirer')
 const inquirerAutoComplete = require('./autocomplete')
+const mpv = require('node-mpv')
+const opsys = process.platform
+const turkanime = require('./api/turkanime')
 
 inquirer.registerPrompt('autocomplete', inquirerAutoComplete)
 
-;(async () => {
+async function main() {
     let animeListesi = await turkanime.animeListesi()
     let animeSor = await inquirer
         .prompt([
@@ -39,5 +41,61 @@ inquirer.registerPrompt('autocomplete', inquirerAutoComplete)
             }
         ])
     let bolumSlug = bolumListesi.get(bolumSor.secilenBolum)
-    console.log(bolumSlug)
-})()
+    
+    let videoListesi = await turkanime.bolumVideolari(bolumSlug)
+    let fansubSor = await inquirer
+        .prompt([
+            {
+                type: 'autocomplete',
+                name: 'secilenFansub',
+                suggestOnly: false,
+                message: 'Bir fansub seçin',
+                searchIn: videoListesi,
+                pageSize: 20,
+                validate: function (val) {
+                    return val ? true : 'Type something!'
+                },
+            }
+        ])
+        secilenFansub = videoListesi.get(fansubSor.secilenFansub)
+        let kaynakSor = await inquirer
+        .prompt([
+            {
+                type: 'autocomplete',
+                name: 'secilenVideo',
+                suggestOnly: false,
+                message: 'Bir alternatif seçin',
+                searchIn: secilenFansub,
+                pageSize: 20,
+                validate: function (val) {
+                    return val ? true : 'Type something!'
+                },
+            }
+        ])
+        let video = secilenFansub.get(kaynakSor.secilenVideo)
+        
+        let mpvAyar = (opsys == "win32" || opsys == "win64") ? {binary: 'mpv.exe'} : {}
+        const mpvOynatici = new mpv(mpvAyar, ['--fps=60'])
+        
+        mpvOynatici.load(video)
+        
+        let baslik = null
+        mpvOynatici.on('statuschange', (durum) => {
+            if (durum['duration'] != null && durum['media-title'] != null) {
+                baslik = mpvOynatici.getProperty('media-title') 
+            }
+        })
+        let yenidenBaslat = 0
+        mpvOynatici.on('stopped', () => {
+            yenidenBaslat++
+            mpvOynatici.stop()
+            if (yenidenBaslat < 2) {
+                console.log('Yeniden basliyoruz')
+                return main()
+            }
+        })
+        if (baslik !== null)
+            console.log(baslik)
+}
+
+main()
